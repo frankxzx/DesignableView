c2bcda80d5daa93ddd3b48da2f5a4275ccbcecf4
frank.xu <frank.xu@touch-spring.com>
2016-03-29 19:31:52 +0800

    push

--- DesignableView/ViewController.swift
+++ DesignableView/ViewController.swift
@@ -23,3 +23,53 @@ class ViewController: UIViewController {
 
 }
 
+@IBDesignable
+class MyCustomView: UIView {
+   
+    @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+        didSet {
+            layer.borderColor = borderColor.CGColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+
+}
+
+class MView: MyCustomView {
+
+}
+
+extension UIView {
+    
+//    @IBInspectable var cornerRadius: CGFloat = 0 {
+//        didSet {
+//            layer.cornerRadius = cornerRadius
+//            layer.masksToBounds = cornerRadius > 0
+//        }
+//    }
+//    
+//    @IBInspectable var borderWidth: CGFloat = 0 {
+//        didSet {
+//            layer.borderWidth = borderWidth
+//        }
+//    }
+//    
+//    @IBInspectable var borderColor: UIColor? {
+//        didSet {
+//            layer.borderColor = borderColor?.CGColor
+//        }
+//    }
+}
+
